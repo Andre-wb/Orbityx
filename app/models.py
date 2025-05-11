7d2de1bf14dd068b54de0846bdd1118a1ca1ed2c
andreykaravaev640 <andreykaravaev640@gmail.com>
2025-05-11 22:03:40 +0800

    Добавление css стилей и js скриптов

--- app/models.py
+++ app/models.py
@@ -8,4 +8,3 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(64), nullable=False)
-
