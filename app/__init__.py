67fc1e815776e25a2f6c8cda7961d8b64d5a9fd6
andreykaravaev640 <andreykaravaev640@gmail.com>
2025-05-11 11:11:55 +0800

    Инициализация Flask Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_login import LoginManager
+from .models import db
+from .routes import main
 from dotenv import load_dotenv
-import os
 load_dotenv()
+import os
+from .models import User
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = os.getenv('SQLALCHEMY_SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'main.introduce_page'
+    login_manager.init_app(app)
+
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    app.register_blueprint(main)
+
+    return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,15 @@
-from flask import render_template, redirect, url_for, request, flash
+from flask import render_template, Blueprint, redirect, url_for
 
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def introduce_page():
+    return render_template('introduce.html', title='Добро пожаловать на Orbityx')
+
+@main.route('/base')
+def base_page():
+    return render_template('base.html')
+
+@main.route('/currency')
+def crypto_currency_page():
+    return render_template('crypto_currency.html', title='Актуальный рейтинг криптовалют на Orbityx')
